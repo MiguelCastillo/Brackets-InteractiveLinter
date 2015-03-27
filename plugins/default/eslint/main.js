6bd23335bafbd9c925190f6f5ae8d43e8d7d5f71
Miguel Castillo <mcastillo@symphono.com>
2015-03-27 17:39:18 -0400

    Initial support for eslint

---
+++ plugins/default/eslint/main.js
@@ -0,0 +1,39 @@
+/**
+ * Interactive Linter Copyright (c) 2014 Miguel Castillo.
+ *
+ * Licensed under MIT
+ */
+
+
+//http://eslint.org/demo/
+define(function(require /*, exports, module*/) {
+    "use strict";
+
+    var utils          = require("libs/utils"),
+        groomer        = require("eslint/groomer"),
+        eslint         = require("eslint/libs/eslint"),
+        defaultOptions = JSON.parse(require("text!eslint/default.json")),
+        settings       = JSON.parse(require("text!eslint/settings.json"));
+
+    function lint(source, options) {
+        options = utils.mixin({}, defaultOptions, options);
+        var results, i, length;
+
+        try {
+            results = eslint.verify(source, options);
+        }
+        catch(ex) {
+            console.log(ex.message);
+        }
+
+        for (i = 0, length = results.length; i < length; i++) {
+            groomer.groom(results[i], options);
+        }
+
+        return results;
+    }
+
+    return utils.mixin(settings, {
+        lint: lint
+    });
+});
