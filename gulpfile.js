72ad24d0075ff73c0e6a48a6075a64f3aa35c723
Stephen Gregory <artcode08@gmail.com>
2015-08-17 15:12:34 -0230

    Retrieve static JSONLint from npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 /*eslint-env node */
 var gulp = require("gulp");
 
-gulp.task("default", ["jshint"], function () {
-    console.log("Installing dependencies");
+gulp.task("default", ["jshint", "jsonlint"], function () {
+    console.log("Installed plugins");
     return;
 });
 
 gulp.task("jshint", function () {
     return gulp.src("./node_modules/jshint/dist/jshint.js")
         .pipe(gulp.dest("./plugins/default/jshint/libs"));
+});
+
+gulp.task("jsonlint", function () {
+    return gulp.src("./node_modules/jsonlint/lib/jsonlint.js")
+        .pipe(gulp.dest("./plugins/default/jsonlint/libs"));
 });
