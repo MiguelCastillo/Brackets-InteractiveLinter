c2bc631a53f551ab0438fd1819bfa17d6dcd8485
Stephen Gregory <artcode08@gmail.com>
2015-08-17 18:04:08 -0230

    Retrieve static requirejs from npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 /*eslint-env node */
 var gulp = require("gulp");
 
-gulp.task("default", ["jshint", "jsonlint", "htmlhint", "jscs", "coffeelint"], function () {
+gulp.task("default", ["jshint", "jsonlint", "htmlhint", "jscs", "coffeelint", "requirejs"], function () {
     console.log("Installed plugins");
     return;
 });
@@ -29,4 +29,9 @@ gulp.task("jscs", function () {
 gulp.task("coffeelint", function () {
     return gulp.src("./node_modules/coffeelint/lib/coffeelint.js")
         .pipe(gulp.dest("./plugins/default/coffeelint/libs"));
+});
+
+gulp.task("requirejs", function () {
+    return gulp.src("./node_modules/requirejs/require.js")
+        .pipe(gulp.dest("./libs/js/"));
 });
